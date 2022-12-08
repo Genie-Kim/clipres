38768ae4dee1c2c8b23dec08e881ed6ca405bafa
Genie-Kim <kimjin928@naver.com>
2022-12-08 18:03:26 +0900

    add no decoder option & clip text enc feezing.

--- model/segmenter.py
+++ model/segmenter.py
@@ -9,10 +9,14 @@ from .layers import FPN, Projector, TransformerDecoder
 
 def freeze_all(model):
     """Freeze params and norm stats."""
-    for m in model.modules():
-        m.eval()
-        for param in m.parameters():
-            param.requires_grad = False
+    try:
+        for m in model.modules():
+            m.eval()
+            for param in m.parameters():
+                param.requires_grad = False
+    except AttributeError:
+        model.requires_grad = False
+                
 
 
 class CRIS(nn.Module):
@@ -24,20 +28,30 @@ class CRIS(nn.Module):
         self.backbone = build_model(clip_model.state_dict(), cfg.word_len).float()
         # Multi-Modal FPN
         self.neck = FPN(in_channels=cfg.fpn_in, out_channels=cfg.fpn_out)
-        # Decoder
-        self.decoder = TransformerDecoder(num_layers=cfg.num_layers,
-                                          d_model=cfg.vis_dim,
-                                          nhead=cfg.num_head,
-                                          dim_ffn=cfg.dim_ffn,
-                                          dropout=cfg.dropout,
-                                          return_intermediate=cfg.intermediate)
+        self.nodecoder = cfg.nodecoder
+        if not self.nodecoder:
+            # Decoder
+            self.decoder = TransformerDecoder(num_layers=cfg.num_layers,
+                                            d_model=cfg.vis_dim,
+                                            nhead=cfg.num_head,
+                                            dim_ffn=cfg.dim_ffn,
+                                            dropout=cfg.dropout,
+                                            return_intermediate=cfg.intermediate)
         # Projector
         self.proj = Projector(cfg.word_dim, cfg.vis_dim // 2, 3)
         self.vispt = True if cfg.visual_prompting is not None else False
         if self.vispt:
             self.clip_visenc = copy.deepcopy(self.backbone.visual)
             del self.clip_visenc.attnpool.connect
+            self.backbone.logit_scale.requires_grad = False
             freeze_all(self.clip_visenc)
+        self.textfreeze = cfg.textfreeze
+        if self.textfreeze:
+            # freeze_all(self.backbone.token_embedding)
+            freeze_all(self.backbone.positional_embedding)
+            freeze_all(self.backbone.transformer)
+            freeze_all(self.backbone.text_projection)
+            self.backbone.logit_scale.requires_grad = False
     
     def forward(self, img, word, mask=None, vp_img = None):
         '''
@@ -47,8 +61,8 @@ class CRIS(nn.Module):
             mask: b, 1, h, w
             vp_img: b, 3, h, w
         '''
-        if self.vispt:
-            if self.training:
+        if self.nodecoder:
+            if self.training and self.vispt:
                 assert vp_img is not None
                 # padding mask used in decoder
                 pad_mask = torch.zeros_like(word).masked_fill_(word == 0, 1).bool()
@@ -58,7 +72,7 @@ class CRIS(nn.Module):
                 vis = self.backbone.encode_image(img)
                 with torch.no_grad():
                     vp_cls = self.clip_visenc(vp_img.type(self.clip_visenc.conv1.weight.dtype),output_cls=True) # b, 512
-                    vp_cls = vp_cls / vp_cls.norm(dim=-1, keepdim=True) * self.backbone.logit_scale # should wrap no grad
+                    vp_cls = vp_cls / vp_cls.norm(dim=-1, keepdim=True) * torch.sqrt(self.backbone.logit_scale) # should wrap no grad
                 # b, 512, 26, 26 (C4)
                 fq = self.neck(vis, vp_cls)
                 b, c, h, w = fq.size()
@@ -81,7 +95,7 @@ class CRIS(nn.Module):
                 # state: b, 1024
                 vis = self.backbone.encode_image(img)
                 _, state = self.backbone.encode_text(word)
-                state = state / state.norm(dim=-1, keepdim=True) * self.backbone.logit_scale
+                state = state / state.norm(dim=-1, keepdim=True) * torch.sqrt(self.backbone.logit_scale)
                 
                 # b, 512, 26, 26 (C4)
                 fq = self.neck(vis, state)
@@ -90,7 +104,15 @@ class CRIS(nn.Module):
                 # b, 1, 104, 104
                 pred = self.proj(fq, state)
 
-                return pred.detach()
+                if self.training:
+                    # resize mask
+                    if pred.shape[-2:] != mask.shape[-2:]:
+                        mask = F.interpolate(mask, pred.shape[-2:],
+                                            mode='nearest').detach()
+                    loss = F.binary_cross_entropy_with_logits(pred, mask)
+                    return pred.detach(), mask, loss
+                else:
+                    return pred.detach()
 
         else:
             # padding mask used in decoder
