7facbc278b06e168ba3c0ad29fa3c5248b59c0c1
Genie-Kim <kimjin928@naver.com>
2022-12-13 14:18:08 +0900

    add perturbation w/ vanilla gaussian & learned gaussian(with mapper training)

--- model/segmenter.py
+++ model/segmenter.py
@@ -17,7 +17,43 @@ def freeze_all(model):
     except AttributeError:
         model.requires_grad = False
                 
+class NoiseMapper(nn.Module):
+    def __init__(self, inputdim=512):
+        super(NoiseMapper, self).__init__()
+        self.linear1 = torch.nn.Linear(inputdim, 1024)
+        self.linear2 = torch.nn.Linear(1024, 1024)
+        self.linear3 = torch.nn.Linear(1024, 1024)
+        self.linear4 = torch.nn.Linear(1024, inputdim)
+        self.linear5 = torch.nn.Linear(inputdim, 1024)
+        self.linear6 = torch.nn.Linear(1024, 1024)
+        self.linear7 = torch.nn.Linear(1024, 1024)
+        self.linear8 = torch.nn.Linear(1024, inputdim)
 
+    def forward(self, x):
+        mu = F.leaky_relu(self.linear1(x))
+        mu = F.leaky_relu(self.linear2(mu))
+        mu = F.leaky_relu(self.linear3(mu))
+        mu = self.linear4(mu)
+        std = F.leaky_relu(self.linear5(x))
+        std = F.leaky_relu(self.linear6(std))
+        std = F.leaky_relu(self.linear7(std))
+        std = self.linear8(std)
+        return mu + std.exp()*(torch.randn(mu.shape).to(x.device))
+    
+    def loss(self, real, fake, temp=0.1, lam=0.5):
+        sim = torch.cosine_similarity(real.unsqueeze(1), fake.unsqueeze(0), dim=-1)
+        if temp > 0.:
+            sim = torch.exp(sim/temp)
+            sim1 = torch.diagonal(F.softmax(sim, dim=1))*temp
+            sim2 = torch.diagonal(F.softmax(sim, dim=0))*temp
+            if 0.<lam < 1.:
+                return -(lam*torch.log(sim1) + (1.-lam)*torch.log(sim2))
+            elif lam == 0:
+                return -torch.log(sim2)
+            else:
+                return -torch.log(sim1)
+        else:
+            return -torch.diagonal(sim)
 
 class CRIS(nn.Module):
     def __init__(self, cfg):
@@ -52,6 +88,15 @@ class CRIS(nn.Module):
             freeze_all(self.backbone.transformer)
             freeze_all(self.backbone.text_projection)
             self.backbone.logit_scale.requires_grad = False
+        self.vispt_perturb = cfg.vispt_perturb
+        if self.vispt_perturb is not None:
+            if self.vispt_perturb =='vanilla_gaussian':
+                self.aug_level = 0.75
+                
+            elif self.vispt_perturb =='learned_perturb':
+                self.mapper = NoiseMapper(512)
+                self.mapper.load_state_dict(torch.load('exp/mapper/CRIS_R101_blur3_addnorm/noise_mapper.pth', map_location='cpu')) # path to the noise mapping network
+                freeze_all(self.mapper)
     
     def forward(self, img, word, mask=None, vp_img = None):
         '''
@@ -73,6 +118,17 @@ class CRIS(nn.Module):
                 with torch.no_grad():
                     vp_cls = self.clip_visenc(vp_img.type(self.clip_visenc.conv1.weight.dtype),output_cls=True) # b, 512
                     vp_cls = vp_cls / vp_cls.norm(dim=-1, keepdim=True) * torch.sqrt(self.backbone.logit_scale) # should wrap no grad
+                    if self.vispt_perturb is not None:
+                        if self.vispt_perturb =='vanilla_gaussian':
+                            random_noise = torch.randn(vp_cls.shape).to(img.device)
+                            random_noise = random_noise/random_noise.norm(dim=-1, keepdim=True)
+                            vp_cls = vp_cls*(1-self.aug_level) + random_noise*self.aug_level
+                            vp_cls = vp_cls / vp_cls.norm(dim=-1, keepdim=True) * torch.sqrt(self.backbone.logit_scale) # should wrap no grad
+                            
+                        elif self.vispt_perturb =='learned_perturb':
+                            vp_cls = self.mapper(vp_cls) + vp_cls
+                            vp_cls = vp_cls / vp_cls.norm(dim=-1, keepdim=True) * torch.sqrt(self.backbone.logit_scale) # should wrap no grad
+
                 # b, 512, 26, 26 (C4)
                 fq = self.neck(vis, vp_cls)
                 b, c, h, w = fq.size()
